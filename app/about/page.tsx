7670ba5089ec6a98ce05b251896da75c82b98763
Pastel <59006202+kimanheng@users.noreply.github.com>
2023-10-17 08:54:16 +0700

    delete

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Image from 'next/image'
-import logo from '.../images/logo.png'
-import gingerbread from '.../images/gingerbread.jpg'
 
 export default async function About(){
     return (
@@ -11,42 +9,6 @@ export default async function About(){
             About
             </h1>
         </div>
-        <ul>
-          <li className="flex item-centers md:item-centers py-4">
-              <div>
-                <Image src={logo} height={160} width={160} alt="Logo"/>
-              </div>
-                <div className="ml-12 w-max">
-                  <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-                    Interact Club
-                  </h3>
-                  <p>
-                    A Student-Led Club <br/> 
-                    <span className ="text-[#7096d1]">
-                        2019-Present</span><br/>
-                    <span className="text-gray-500 dark:text-gray-400 line-clamp-2">
-                    For everyone and the community.</span>
-                </p>
-              </div>    
-          </li>
-          <li className="flex item-centers md:item-centers py-4">
-              <div>
-                <Image className="rounded-full" src={gingerbread} height={160} width={160} alt="Gingerbread"/>
-              </div>
-                <div className="ml-12 w-max">
-                  <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-                    Ginger Bread
-                  </h3>
-                  <p>
-                    Vice President <br/> 
-                    <span className ="text-[#7096d1]">
-                        2023-2024</span><br/>
-                    <span className="text-gray-500 dark:text-gray-400 line-clamp-2">
-                        For everyone and the community.</span>
-                </p>
-              </div>    
-          </li>
-      </ul>
     </div>
     )
 }
