3512ba29eacec1aa8d53846439bdc10d9c63a7c3
Pastel <59006202+kimanheng@users.noreply.github.com>
2023-10-17 08:52:30 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function IndexPage() {
     return (
         <div className="divide-y divide-gray-400 dark:divide-gray-700">
             <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-                <h1 className="text-3xl font-semibold text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                <h1 className="text-3xl font-medium text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                     INTERACT <span className="text-[#7096d1]">CLUB</span>
         </h1>
       </div>
