d560caddea96ef6a0e61b66386739e7b7f1af1e3
Pastel <59006202+kimanheng@users.noreply.github.com>
2023-10-19 19:22:14 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,13 @@ export default function IndexPage() {
                   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><h1 className="text-6xl font-medium text-[#081f3c]">
                  INTERACT <span className="text-[#7096d1]">CLUB</span></h1></a>
               </div>
-              <div className="flex flex-wrap justify-center mt-8">
-                        {row0.map((row, i) => <ImageBox src={row.src} width={row.width} height={row.height}></ImageBox>)}
+              <div className="flex flex-wrap mt-8 w-full">
+              <div className="justify-center md:scale-95 md:flex">{row0.map((row, i) => <ImageBox src={row.src} width={row.width} height={row.height}></ImageBox>)}
 
-                    </div>
-                    <div className="flex flex-wrap justify-center mt-8">
-                        {row1.map((row, i) => <ImageBox src={row.src} width={row.width} height={row.height}></ImageBox>)}
-                    </div>
+                    </div></div>
+                    <div className="flex flex-wrap w-full">
+                        <div className="justify-center md:scale-95 md:flex">{row1.map((row, i) => <ImageBox src={row.src} width={row.width} height={row.height}></ImageBox>)}
+                    </div></div>
                 </div>
         <h1 className="text-xl font-light text-center text-[#081f3c] inset-x-0 bottom-0 h-16">
                 The Interact Club is a student-led club that aims to help those in need and urge others to do the same.
