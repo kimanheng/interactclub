ffda75758f3f9147f307d18dbdc9ed43615cc348
Pastel <59006202+kimanheng@users.noreply.github.com>
2024-02-12 19:53:07 +0700

    update version

--- app/horror/page.tsx
+++ app/horror/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import jumpscare from 'images/jumpscare.jpg'
 
+
 export default function Jumpscare() {
     return (
         <div className="absolute top-0 left-0 w-max h-max z-50">
